1e30aba45d67f8cf090709aedc657d2d9c42c883
Matt Rider <mattwr18@gmail.com>
2018-11-13 17:27:02 -0200

    Install WikiSEO with composer

--- install-update-php-dependencies.sh
+++ install-update-php-dependencies.sh
@@ -1,2 +1,3 @@
 php composer.phar require mediawiki/semantic-media-wiki "3.0.0" --update-no-dev
+php composer.phar require tinymighty/wiki-seo "1.2.2" --update-no-dev
 php composer.phar update --no-dev
