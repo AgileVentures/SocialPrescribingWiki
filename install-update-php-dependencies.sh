289a81af1d209ce1b8695a8aecdf42563578cb95
Matt Rider <mattwr18@gmail.com>
2019-02-11 15:05:20 -0200

    Debug net_smtp

--- install-update-php-dependencies.sh
+++ install-update-php-dependencies.sh
@@ -1,2 +1,3 @@
 COMPOSER_ALLOW_SUPERUSER=true php composer.phar require mediawiki/semantic-media-wiki "3.0.0" --update-no-dev
+COMPOSER_ALLOW_SUPERUSER=true php composer.phar require pear/net_smtp "1.8.1" --update-no-dev
 COMPOSER_ALLOW_SUPERUSER=true php composer.phar update --no-dev
